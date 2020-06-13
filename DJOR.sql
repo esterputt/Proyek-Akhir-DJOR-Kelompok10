03b02899aee8637a823d878f8113cf992ba48c9d
Grace Panjaitan <60679474+gracepanjaitan@users.noreply.github.com>
2020-06-13 10:12:42 +0700

    Update DJOR.sql

--- DJOR.sql
+++ DJOR.sql
@@ -1,34 +1,33 @@
-CREATE DATABASE DJOR;
 
-CREATE TABLE login_petugas (
-username VARCHAR(50) NOT NULL,
+CREATE TABLE login (
+username VARCHAR(50) NOT NULL, 
 passwordd VARCHAR(50) NOT NULL);
+
+INSERT INTO login(username, passwordd) VALUES ('grace.pjtn', 'grace1990');
+INSERT INTO login(username, passwordd) VALUES ('djorrr', '123456789');
+SELECT * FROM login WHERE username='grace.pjtn';
 
 CREATE TABLE transaksi(
 nomor_transaksi INT PRIMARY KEY,
-tgl_parkir DATE,
-waktu_mulai TIME,
-waktu_selesai TIME,
+tgl_parkir DATE, 
+waktu_mulai, waktu_selesai TIME,
 lama_parkir INT,
 biaya_parkir LONG INT,
-jenis INT,
+
+jeniskendaraan INT,
 nomorslot INT,
-slot CHAR,
-CONSTRAINT transaksi_pk PRIMARY KEY(jenis_, nomorslot, slot_),
+slot_parkir CHAR,
+
 CONSTRAINT 'fk_jenis_transaksi'
-	FOREIGN KEY (jenis_) REFERENCES jenis_kendaraan(jenis)
+	FOREIGN KEY (jeniskendaraan) REFERENCES jenis_kendaraan(jenis)
 	ON DELETE CASCADE
 	ON UPDATE RESTRICT
 	
 CONSTRAINT 'fk_transaksi_lokasi'
-	FOREIGN KEY (nomorslot) REFERENCE lokasi_parkir(nomor_slot)
+	FOREIGN KEY (nomorslot, slot_parkir) REFERENCE lokasi_parkir(nomor_slot,slot)
 	ON DELETE CASCADE
 	ON UPDATE RESTRICT
 
-CONSTRAINT 'fk_transaksi_lokasi'
-	FOREIGN KEY (slot_) REFERENCE lokasi parkir(slot)
-	ON DELETE CASCADE
-	ON UPDATE RESTRICT
 )
 
 CREATE TABLE jenis_kendaraan( 
@@ -40,5 +39,8 @@ DROP TABLE jenis_kendaraan
 CREATE TABLE lokasi_parkir(
 slot CHAR,
 nomor_slot INT,
-status_ BOOLEAN,
-CONSTRAINT lokasi_parkir_pk PRIMARY KEY (slot, nomor_slot));
+status_slot BOOLEAN,
+lokasi_parkir_pk PRIMARY KEY (slot, nomor_slot));
+
+DROP TABLE jenis_kendaraan
+DROP TABLE lokasi_parkir
